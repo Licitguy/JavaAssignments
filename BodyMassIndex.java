bea06645f046bf1351d9f8e6823df69301a4e534
Licitguy <bolajih511@gmail.com>
2022-12-23 11:29:10 +0100

    java

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -2,19 +2,19 @@
 import java.util.Scanner;
 
     public class BodyMassIndex {
+     private static double weight;
+       private static double height;
+       private static int pounds;
 
-    public static void main(String[] args) {
-            double weight;
-            double height;
 
-
+        public static void main(String[] args) {
 
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter weight in pounds: ");
         double userInput = input.nextDouble();
-        bmi = weight*703(height/height)
-        System.out.println("your bmi is %.2f%n" , bmi);
+        double bmi = weight*703/(height * height);
+        System.out.printf("your bmi is %.2f%n", bmi);
         if (userInput < 18.5) {
             System.out.println("is underweight");
         }
