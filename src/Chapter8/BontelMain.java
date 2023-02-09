010f21c0493c457c4fa3884e3d53268e15be7c68
Licitguy <bolajih511@gmail.com>
2023-02-09 12:39:15 +0100

    My_work

--- src/Chapter8/BontelMain.java
+++ src/Chapter8/BontelMain.java
@@ -1,27 +1,30 @@
-//package Bontel;
-//
-//import java.util.Scanner;
-//
-//public class BontelMain {
-//
-//    public static void main(String[] args) {
-//
-//
-//        System.out.println(Bontel.displayMenu());
-//        Bontel.userSelection();
-//        Bontel.decideUserInput();
-//
-//
-////            System.out.println("Do you wish to continue? yes/no");
-////            String userChoice = input.next();
-////
-////            while(userChoice.equalsIgnoreCase("yes")){
-////                System.out.println(bontel.displayMenu());
-////                bontel.userSelection();
-////                System.out.println("Do you wish to continue? yes/no");
-////                userChoice = input.next();
-////            }
-////            System.out.println("Thank you");
-////    }
-//    }
-//}
+/*
+package Bontel;
+import Chapter8.Bontel;
+
+import java.util.Scanner;
+
+import static Chapter8.Bontel.input;
+
+public class BontelMain {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(Bontel.displayMenu());
+        Bontel.userSelection();
+        Bontel.decideUserInput();
+
+
+        System.out.println("Do you wish to continue? yes/no");
+        String userChoice = input.next();
+
+        while (userChoice.equalsIgnoreCase("yes")) {
+            System.out.println(bontel.displayMenu());
+            bontel.userSelection();
+            System.out.println("Do you wish to continue? yes/no");
+            userChoice = input.next();
+        }
+        System.out.println("Thank you");
+    }
+}*/
